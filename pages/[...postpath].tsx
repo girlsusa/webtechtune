e6d44023d24345440feb58ab262094ef938cdebb
girlsusa <133983794+girlsusa@users.noreply.github.com>
2023-05-20 21:37:46 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://webtechtune.com/` + encodeURI(path as string)
+					`https://webtechtune.com/`
 				}`,
 			},
 		};
